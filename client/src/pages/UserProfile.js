13f661769315883fed3354dc1f70ecf72396667f
DEVESH PRATAP SINGH <152188786+Devesh639281@users.noreply.github.com>
2025-03-26 10:23:13 +0530

    Update UserProfile.js

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Layout } from "../component/Layout";
 import { Form, Input, Row, Col, message } from "antd";
 import { useParams, useNavigate } from "react-router-dom";
@@ -7,20 +7,18 @@ import axios from "axios";
 
 const UserProfile = () => {
   const { user } = useSelector((state) => state.user);
-     // eslint-disable-next-line
-  const [users, setUsers] = useState();
   const params = useParams();
   const navigate = useNavigate();
 
-  const getUserInfo = async () => {
+  const getUser Info = async () => {
     try {
-      const res = await axios.get(`/api/v1/user/getUserInfo/${user._id}`, {
+      const res = await axios.get(`/api/v1/user/getUser Info/${user._id}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
       if (res.data.success) {
-        setUsers(res.data.data);
+        // You can set the user data directly in the form if needed
       }
     } catch (error) {
       console.log(error);
@@ -28,14 +26,14 @@ const UserProfile = () => {
   };
 
   useEffect(() => {
-    getUserInfo();
-    // eslint-disable-next-line
-  }, [params._id]);
+    getUser Info();
+    // Include getUser Info in the dependency array
+  }, [user._id]); // Use user._id to ensure it runs when user changes
 
   const onFinishHandler = async (values) => {
     try {
       const res = await axios.post(
-        `/api/v1/user/updateUserProfile/${params._id}`,
+        `/api/v1/user/updateUser Profile/${params._id}`,
         values,
         {
           headers: {
@@ -63,15 +61,17 @@ const UserProfile = () => {
           className="m-3 form-layout"
           initialValues={{
             ...user,
-          }}>
+          }}
+        >
           <h4>Personal Details:</h4>
           <Row gutter={20} className="form-input-padding">
             <Col xs={24} md={24} lg={8}>
               <Form.Item
                 label="Id"
                 name="_id"
-                rules={[{ required: true, message: "_id Here" }]}>
-                <Input type="text" placeholder="Not Changable" />
+                rules={[{ required: true, message: "_id Here" }]}
+              >
+                <Input type="text" placeholder="Not Changeable" disabled />
               </Form.Item>
             </Col>
 
@@ -80,9 +80,10 @@ const UserProfile = () => {
                 label="Name"
                 name="name"
                 rules={[
-                  { required: true, message: "Please enter your first name" },
-                ]}>
-                <Input type="text" placeholder="Your first Name" />
+                  { required: true, message: "Please enter your name" },
+                ]}
+              >
+                <Input type="text" placeholder="Your Name" />
               </Form.Item>
             </Col>
 
@@ -91,21 +92,20 @@ const UserProfile = () => {
                 label="Answer"
                 name="answer"
                 rules={[
-                  { required: true, message: "Please enter your last name" },
-                ]}>
-                <Input type="text" placeholder="Your Last Name" />
+                  { required: true, message: "Please enter your answer" },
+                ]}
+              >
+                <Input type="text" placeholder="Your Answer" />
               </Form.Item>
             </Col>
+
             <Col xs={24} md={24} lg={8}>
               <Form.Item
-                label="isDoctor"
+                label="Is Doctor"
                 name="isDoctor"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}>
-                <Input type="text" placeholder="isDoctor" />
+                rules={[{ required: true, message: "Please specify if a doctor" }]}
+              >
+                <Input type="text" placeholder="Is Doctor" />
               </Form.Item>
             </Col>
 
@@ -118,23 +118,26 @@ const UserProfile = () => {
                     required: true,
                     message: "Please enter your email address",
                   },
-                ]}>
+                ]}
+              >
                 <Input type="email" placeholder="Your Email Address" />
               </Form.Item>
             </Col>
+
             <Col xs={24} md={24} lg={8}>
               <Form.Item
-                label="isAdmin"
+                label="Is Admin"
                 name="isAdmin"
-                rules={[{ required: true, message: "Please enter Password" }]}>
-                <Input type="text" placeholder="Your Password" />
+                rules={[{ required: true, message: "Please specify if an admin" }]}
+              >
+                <Input type="text" placeholder="Is Admin" />
               </Form.Item>
             </Col>
 
             <Col xs={24} md={24} lg={8}>
               <div className="d-flex justify-content-end">
                 <button className="btn btn-primary form-btn" type="submit">
-                  UpdateUserProfile
+                  Update User Profile
                 </button>
               </div>
             </Col>
