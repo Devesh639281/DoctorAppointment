fa61fb4fc8cf349796cc1d9f04933ba5d1a208f0
DEVESH PRATAP SINGH <152188786+Devesh639281@users.noreply.github.com>
2025-03-27 00:11:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,9 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(`mongodb+srv://rabbit956516:devesh9565
+@doctapt.1me3oyc.mongodb.net/DoctAppDbName?retryWrites=true&w=majority&appName=DoctAPT`);
+
     console.log(`Mongo Connected ${mongoose.connection.host}`.bgRed.blue);
   } catch (error) {
     console.log(`Mongoose Server Issues ${error}`.bgRed.white);
